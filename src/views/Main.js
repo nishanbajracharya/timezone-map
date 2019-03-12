5c4db7f981f5341a5562c278f1a0da18f4207534
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-03-12 11:10:08 +0545

    Add gh-pages

--- src/views/Main.js
+++ src/views/Main.js
@@ -3,21 +3,35 @@ import React from 'react';
 import Map from '../components/Map';
 
 class Main extends React.Component {
+  state = {
+    selected: {}
+  };
 
   getMapStyles = geography => {
     return {
-      default: { fill: '#ddd', stroke: '#999', outline: 'none' },
+      default: {
+        fill: this.state.selected.id === geography.id ? '#47a' : '#ddd',
+        stroke: '#999',
+        outline: 'none'
+      },
       hover: { fill: '#49c', stroke: '#47a', outline: 'none' },
       pressed: { fill: '#47a', stroke: '#458', outline: 'none' }
     };
-  }
+  };
 
   handleClick = geography => {
-    console.log(geography)
-  }
+    this.setState(prevState => ({
+      selected: prevState.selected.id !== geography.id ? geography : {}
+    }));
+  };
 
   render() {
-    return <div><Map getStyles={this.getMapStyles} onClick={this.handleClick} /></div>;
+    return (
+      <div>
+        <Map getStyles={this.getMapStyles} onClick={this.handleClick} />
+        <p>{this.state.selected.id}</p>
+      </div>
+    );
   }
 }
 
