0337e8ba087f5418793d7e622d0bd88f9ce3c97a
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-03-12 10:51:15 +0545

    Move styles

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,36 +8,25 @@ import {
 
 import geography from '../constants/geography.json';
 
-class Map extends React.Component {
-  getStyles = geography => {
-    return {
-      default: { fill: '#ddd', stroke: '#999', outline: 'none' },
-      hover: { fill: '#49c', stroke: '#47a', outline: 'none' },
-      pressed: { fill: '#47a', stroke: '#458', outline: 'none' }
-    };
-  };
-
-  render() {
-    return (
-      <div>
-        <ComposableMap viewBox="0 0 960 1008">
-          <ZoomableGroup>
-            <Geographies geography={geography} disableOptimization>
-              {geographies =>
-                geographies.map(geography => (
-                  <Geography
-                    key={geography.id}
-                    geography={geography}
-                    style={this.getStyles(geography)}
-                  />
-                ))
-              }
-            </Geographies>
-          </ZoomableGroup>
-        </ComposableMap>
-      </div>
-    );
-  }
-}
+const Map = props => (
+  <div className="map-container">
+    <ComposableMap height="800" viewBox="0 0 960 1008">
+      <ZoomableGroup>
+        <Geographies geography={geography} disableOptimization>
+          {geographies =>
+            geographies.map(geography => (
+              <Geography
+                key={geography.id}
+                geography={geography}
+                onClick={props.onClick}
+                style={props.getStyles(geography)}
+              />
+            ))
+          }
+        </Geographies>
+      </ZoomableGroup>
+    </ComposableMap>
+  </div>
+);
 
 export default Map;
--- src/views/Main.js
+++ src/views/Main.js
@@ -3,8 +3,21 @@ import React from 'react';
 import Map from '../components/Map';
 
 class Main extends React.Component {
+
+  getMapStyles = geography => {
+    return {
+      default: { fill: '#ddd', stroke: '#999', outline: 'none' },
+      hover: { fill: '#49c', stroke: '#47a', outline: 'none' },
+      pressed: { fill: '#47a', stroke: '#458', outline: 'none' }
+    };
+  }
+
+  handleClick = geography => {
+    console.log(geography)
+  }
+
   render() {
-    return <div><Map /></div>;
+    return <div><Map getStyles={this.getMapStyles} onClick={this.handleClick} /></div>;
   }
 }
 
